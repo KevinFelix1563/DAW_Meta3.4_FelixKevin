4aaa34a41f7162fa4f2b1600562c0fda2cf4864c
Kevin Felix <kevin.felix59@uabc.edu.mx>
2026-04-28 00:37:28 -0700

    feat: SSL/TLS implementado en backend

--- backend_tareas/server.js
+++ backend_tareas/server.js
@@ -1,7 +1,19 @@
 import app from './src/app.js';
+import express from 'express';
+import https from 'https';
+import fs from 'fs';
+import cors from 'cors';
+
+// Cargar los certificados SSL/TLS
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+// Creamos el servidor HTTPS
+const server = https.createServer(options, app);
+server.listen(PORT, () => {
+  console.log(`🚀 Servidor seguro corriendo en: https://localhost:${PORT}`);
 });
