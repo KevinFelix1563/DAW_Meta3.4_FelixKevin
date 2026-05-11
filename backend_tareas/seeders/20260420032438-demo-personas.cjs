03dd94f7ec63a2807c57126fe8bf0620297a9c99
Kevin Felix <kevin.felix59@uabc.edu.mx>
2026-05-11 14:05:18 -0700

    feat: Uso de bcryptjs para manejar passwords seguras

--- backend_tareas/seeders/20260420032438-demo-personas.cjs
+++ backend_tareas/seeders/20260420032438-demo-personas.cjs
@@ -1,12 +1,53 @@
 'use strict';
+const bcrypt = require('bcryptjs'); // Usaremos bcryptjs para el cifrado
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // Generamos una contraseña encriptada por defecto para las pruebas
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash('secreto123', salt);
+
     await queryInterface.bulkInsert('Personas', [
-      { id: 1, nombre: 'Kevin Felix', email: 'kevin@uabc.edu.mx', activo: true, createdAt: new Date(), updatedAt: new Date() },
-      { id: 2, nombre: 'Adad Arias', email: 'adad@uabc.edu.mx', activo: true, createdAt: new Date(), updatedAt: new Date() },
-      { id: 3, nombre: 'Dante Palacios', email: 'dante@uabc.edu.mx', activo: false, createdAt: new Date(), updatedAt: new Date() },
-      {id: 4, nombre: 'Kevin Felix', email: 'kevin.felix59@uabc.edu.mx', activo: true, createdAt: new Date(), updatedAt: new Date()}
+      { 
+        id: 1, 
+        nombre: 'Kevin Felix', 
+        email: 'kevin@uabc.edu.mx', 
+        password: hashedPassword,
+        rol: 'usuario',
+        activo: true, 
+        createdAt: new Date(), 
+        updatedAt: new Date() 
+      },
+      { 
+        id: 2, 
+        nombre: 'Adad Arias', 
+        email: 'adad@uabc.edu.mx', 
+        password: hashedPassword,
+        rol: 'usuario',
+        activo: true, 
+        createdAt: new Date(), 
+        updatedAt: new Date() 
+      },
+      { 
+        id: 3, 
+        nombre: 'Dante Palacios', 
+        email: 'dante@uabc.edu.mx', 
+        password: hashedPassword,
+        rol: 'usuario',
+        activo: false, 
+        createdAt: new Date(), 
+        updatedAt: new Date() 
+      },
+      {
+        id: 4, 
+        nombre: 'Admin Kevin', 
+        email: 'kevin.felix59@uabc.edu.mx', 
+        password: hashedPassword,
+        rol: 'admin', // <-- ESTE ES TU USUARIO ADMINISTRADOR
+        activo: true, 
+        createdAt: new Date(), 
+        updatedAt: new Date()
+      }
     ], {});
   },
 
